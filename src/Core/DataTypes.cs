7cf82bfb7434676105fe35ae4dcd39603e8b6c49
Kirill Smirenko <cyrussmith1865@gmail.com>
2015-11-10 00:49:05 +0300

    Written Core, not tested.

---
+++ src/Core/DataTypes.cs
@@ -0,0 +1,32 @@
+﻿namespace Core {
+    public enum PointLocation {
+        Inside,
+        Outside,
+        OnBorder,
+        OnVertex
+    }
+
+    public class Point {
+        public Point(double x, double y) {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; private set; }
+        public double Y { get; private set; }
+    }
+
+    public class Line {
+        public Line(Point start, Point vector) {
+            Start = start;
+            Vector = vector;
+        }
+
+        public static Line FromPoints(Point start, Point end) {
+            return new Line(start, new Point(end.X - start.X, end.Y - start.Y));
+        }
+
+        public Point Start { get; private set; }
+        public Point Vector { get; private set; }
+    }
+}
---
+++ src/Core/Logic.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Core {
+    public class TriangleCheck {
+        private const int IntersectTrue = 1;
+        private const int IntersectFalse = -1;
+        private const int IntersectVertex = 0;
+        private const double Tolerance = 1e-9;
+
+        public static PointLocation CalcPointLocation(Point a, Point b, Point c,
+            Point p) {
+            var sides = new Line[3] {
+                Line.FromPoints(a, b), Line.FromPoints(b, c),
+                Line.FromPoints(c, a)
+            };
+            if (CheckPointOnSegment(p, sides[0]) ||
+                CheckPointOnSegment(p, sides[1]) ||
+                CheckPointOnSegment(p, sides[2])) return PointLocation.OnBorder;
+            var vectors = new Point[4] {
+                new Point(1f, 0f), new Point(0f, 1f), new Point(-1f, 0f),
+                new Point(0f, -1f)
+            };
+            foreach (var vector in vectors) {
+                var ray = new Line(p, vector);
+                var intersectionsCount = 0;
+                var nextVector = false;
+                foreach (var side in sides) {
+                    switch (CheckRaySegmentIntersection(ray, side)) {
+                        case IntersectVertex:
+                            nextVector = true;
+                            break;
+                        case IntersectTrue:
+                            intersectionsCount++;
+                            break;
+                    }
+                }
+                if (nextVector) break;
+                return (intersectionsCount % 2 == 0)
+                    ? PointLocation.Outside
+                    : PointLocation.Inside;
+            }
+            return PointLocation.Outside;
+        }
+
+        private static bool CheckPointOnSegment(Point point, Line segment) {
+            return (point.X - segment.Start.X) / segment.Vector.X -
+                   (point.Y - segment.Start.Y) / segment.Vector.Y < Tolerance;
+        }
+
+        private static int CheckRaySegmentIntersection(Line ray, Line segment) {
+            var a = ray.Vector.X;
+            var b = -segment.Vector.X;
+            var c = ray.Vector.Y;
+            var d = -segment.Vector.Y;
+            var e = segment.Start.X - ray.Start.X;
+            var f = segment.Start.Y - ray.Start.Y;
+
+            var determinant = a * d - b * c;
+            if (!(Math.Abs(determinant) > Tolerance)) return IntersectFalse;
+            var t = (e * d - b * f) / determinant;
+            var s = (a * f - e * c) / determinant;
+            if (!(t >= 0.0)) return IntersectFalse;
+            if (s > 0.0 && s < 1.0) {
+                return IntersectTrue;
+            }
+            if (Math.Abs(s) < Tolerance || Math.Abs(s) - 1.0 < Tolerance) {
+                return IntersectVertex;
+            }
+            return IntersectFalse;
+        }
+    }
+}
