27f52cd5d064ac0d5e3aca9c20d461d230f88efb
Alexander Chebykin <a.e.chebykin@gmail.com>
2015-11-12 20:05:33 +0300

    Bat files updated to work properly.

--- getNuget.bat
+++ getNuget.bat
@@ -2,7 +2,6 @@
 
 if exist "NuGet.exe" goto :packagesRestore
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://nuget.org/nuget.exe', 'NuGet.exe')"
-goto :packagesRestore
 
 :packagesRestore
 "NuGet.exe" "restore" "TriangleCheck.sln" "-OutputDirectory" "packages"
--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 @echo off
+
 if exist ".git" goto :repositoryExists
 rem Git doesn't allow to clone into non-empty directoty
 git.exe clone --progress -v "https://github.com/ksmirenko/triangle-check" %cd%\temp > log.txt 2>&1
@@ -6,28 +7,33 @@ xcopy /e /q /h /y %cd%\temp %cd%
 goto :build
 
 :repositoryExists
-if not exist out goto :packagesExist
+if not exist out goto :checkPackages
 echo A current build exists.
-set /p delBuild=Do you want to overwrite it [y/n]?: 
+set /p delBuild=Do you want to overwrite it [n if not, anything otherwise]?: 
 if %delBuild% == n goto :EOF
 rmdir /s /q out
 rm log.txt
-goto :packagesExist
 
-:packagesExist
+:checkPackages
 if exist packages goto :update
 echo Downloading necessary packages
-getNuget.bat
-goto :update
+call getNuget.bat
 
 :update
 echo Updating source code
 git.exe pull --progress -v "https://github.com/ksmirenko/triangle-check" >> log.txt 2>&1
-goto :build
 
 :build
 mkdir out
 echo Building solution
 msbuild "%cd%\TriangleCheck.sln" /p:Configuration=Release /p:OutDir=..\..\out /p:TargetFramework=v4.5.1 >> log.txt 2>&1
+for /f %%i in (%cd%\expectedFiles.txt) do (
+    if not "%cd%\out\%%i" exist 
+    echo %%i haven't been created! >> log.txt 2>&1
+    )
 
-sendEmail.bat
+:test
+"%cd%\packages\NUnit.Runners.2.6.4\tools\nunit-console" "%cd%\out\Test.dll" >> log.txt 2>&1
+
+:sendEmail
+call sendEmail.bat
