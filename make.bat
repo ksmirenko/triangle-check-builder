641439f3ae2960eaad202dd591bda367c12d0d90
Kirill Smirenko <cyrussmith1865@gmail.com>
2015-11-12 20:45:15 +0300

    SendMail now doesn't write everything into the console; build check fixed, but expectedFiles.txt needed.

--- make.bat
+++ make.bat
@@ -16,23 +16,24 @@ rm log.txt
 
 :checkPackages
 if exist packages goto :update
-echo Downloading necessary packages
+echo Downloading necessary packages...
 call getNuget.bat
 
 :update
-echo Updating source code
+echo Updating source code...
 git.exe pull --progress -v "https://github.com/ksmirenko/triangle-check" >> log.txt 2>&1
 
 :build
 mkdir out
-echo Building solution
+echo Building solution...
 msbuild "%cd%\TriangleCheck.sln" /p:Configuration=Release /p:OutDir=..\..\out /p:TargetFramework=v4.5.1 >> log.txt 2>&1
+echo Checking build...
 for /f %%i in (%cd%\expectedFiles.txt) do (
-    if not "%cd%\out\%%i" exist 
-    echo %%i haven't been created! >> log.txt 2>&1
+    if not exist "%cd%\out\%%i" echo %%i haven't been created! >> log.txt 2>&1
     )
 
 :test
+echo Testing...
 "%cd%\packages\NUnit.Runners.2.6.4\tools\nunit-console" "%cd%\out\Test.dll" >> log.txt 2>&1
 
 :sendEmail
--- sendEmail.bat
+++ sendEmail.bat
@@ -1,5 +1,6 @@
+echo Sending email...
 @echo off
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://mailsend.googlecode.com/files/mailsend1.17b14.exe', 'mailsend.exe')"
 for /f "delims=" %%A in (%cd%\emailProperties.txt) do set %%A
-%cd%\mailsend.exe -to %to% -from %from% -ssl -port 465 -auth -smtp %smtp% -sub %sub% +cc +bc -v -user %user% -pass %pass% -attach %attach%
+call %cd%\mailsend.exe -to %to% -from %from% -ssl -port 465 -auth -smtp %smtp% -sub %sub% +cc +bc -v -user %user% -pass %pass% -attach %attach% >nul
 pause
