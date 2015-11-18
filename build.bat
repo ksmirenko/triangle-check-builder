8074e97ad6cb3ebd8df59fb2f4b0740158686ee3
SergeevED <sergeev.ev.dm@gmail.com>
2015-11-18 13:05:06 -0800

    changed target framwork in build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 if "%main%"=="" goto :EOF
 mkdir out
 echo Building solution...
-"%MSBUILD%" "%cd%\%SOLUTION_NAME%" /p:Configuration=Release /p:OutDir=%OUTDIR% /p:TargetFramework=v4.5.1 /p:ToolsDllPath=%TOOLS_DLL_PATH% >> log.txt 2>&1
+"%MSBUILD%" "%cd%\%SOLUTION_NAME%" /p:Configuration=Release /p:OutDir=%OUTDIR% /p:TargetFramework=v4.5.2 /p:ToolsDllPath=%TOOLS_DLL_PATH% >> log.txt 2>&1
 if ERRORLEVEL 1 (
 	set error=Build_failure
     echo ERROR: Build failed!
